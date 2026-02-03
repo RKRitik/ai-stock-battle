86ef51a39801be3803d16cf829d48aee0884ce96
RKRitik <khatri.ritik16@gmil.com>
2026-02-03 15:59:37 +0530

    fix: chart styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-// import Chart from "@/components/molecules/chart";
 import AgentsDashboard from "./(dashboard)/agents-dashboard";
 import TransactionsDashboard from "./(dashboard)/transactions-dashboard";
 import HistoryDashboard from "./(dashboard)/history-dashboard";
@@ -7,13 +6,18 @@ export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <AgentsDashboard />
-        <HistoryDashboard />
-        <TransactionsDashboard />
+    <div className="flex min-h-screen justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full flex-col py-20 bg-white dark:bg-black">
+        <div className="w-full max-w-4xl mx-auto px-6">
+          <AgentsDashboard />
+        </div>
+        <div className="w-full max-w-6xl mx-auto px-6 mt-12">
+          <HistoryDashboard />
+        </div>
+        <div className="w-full max-w-4xl mx-auto px-6 mt-12">
+          <TransactionsDashboard />
+        </div>
       </main>
     </div>
   );
 }
-
