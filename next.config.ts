324494474e53b1f8ca593645e610407a3da0ee95
RKRitik <khatri.ritik16@gmail.com>
2026-02-10 13:24:54 +0530

    feat: improve security headers

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,27 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://ritikkhatri.dev https://*.ritikkhatri.dev;",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
